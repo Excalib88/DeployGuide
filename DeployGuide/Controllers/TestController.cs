e41b0c77173f02e4ec36735a1359f65407a9ac5f
Damir Mansurov <d.mansurov@mandarin.io>
2023-11-07 16:31:15 +0300

    use redis for users

--- DeployGuide/Controllers/TestController.cs
+++ DeployGuide/Controllers/TestController.cs
@@ -1,5 +1,7 @@
+using System.Text.Json;
 using DeployGuide.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace DeployGuide.Controllers;
 
@@ -8,17 +10,36 @@ namespace DeployGuide.Controllers;
 public class TestController: ControllerBase
 {
     private readonly DataContext _context;
+    private readonly IDistributedCache _cache;
 
-     public TestController(DataContext context)
+     public TestController(DataContext context, IDistributedCache cache)
      {
          _context = context;
+         _cache = cache;
      }
 
-    [HttpGet]
-    public IActionResult Test()
+     [HttpGet]
+     public async Task<IActionResult> Test()
+     {
+         var user = _context.Users.FirstOrDefault();
+
+         if (user == null) return BadRequest("User not found");
+
+         await _cache.SetStringAsync(user.Id.ToString(), JsonSerializer.Serialize(user), new DistributedCacheEntryOptions
+         {
+             AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10)
+         });
+
+     return Ok("Hello world!" + $"{user?.Id} {user?.Name} {user?.Email}");
+    }
+
+    [HttpGet("redis/{id}")]
+    public async Task<IActionResult> TestRedis(string id)
     {
-        var user = _context.Users.FirstOrDefault();
-        return Ok("Hello world!" + $"{user?.Id} {user?.Name} {user?.Email}");
+        var user = await _cache.GetStringAsync(id);
+        var userEntity = JsonSerializer.Deserialize<UserEntity>(user);
+        
+        return Ok($"From redis: {userEntity?.Id} {userEntity?.Email} {userEntity?.Name} userstring: {user}");
     }
 
     [HttpPost]
